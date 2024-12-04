ca3ff2516db478513e87a696ddbebae994bdc95a
JohPaulMagana <smain0921@gmail.com>
2024-12-04 13:16:14 +0800

    Create MyActionListener.java

---
+++ Pre-Final Activity/MyActionListener.java
@@ -0,0 +1,10 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyActionListener  implements ActionListener {
+	public void actionPerformed (ActionEvent e) {
+		Toolkit.getDefaultToolkit().beep();
+	}
+}
